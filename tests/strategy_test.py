ec8c33fbc1d4c08aacb98733eec238277b81cf49
Alexander Dejanovski <alex@thelastpickle.com>
2020-02-12 17:59:06 +0100

    Remove nodetool ring calls and remove the tokens from the Host objects

--- tests/strategy_test.py
+++ tests/strategy_test.py
@@ -23,10 +23,10 @@ from cstar.strategy import Strategy, find_next_host, HostIsDown
 def make_topology(size, has_down_host=False):
     test_topology = []
     for i in range(size):
-        test_topology.append(Host("a", "1.2.3.%d" % i, "eu", "cluster1", i * 100, not has_down_host, "hostId"))
-        test_topology.append(Host("b", "2.2.3.%d" % i, "us", "cluster1", (i * 100) + 1, True, "hostId"))
-        test_topology.append(Host("c", "3.2.3.%d" % i, "eu", "cluster2", i * 100, True, "hostId"))
-        test_topology.append(Host("d", "4.2.3.%d" % i, "us", "cluster2", (i * 100) + 1, True, "hostId"))
+        test_topology.append(Host("a", "1.2.3.%d" % i, "eu", "cluster1", "rac{}".format(i % 3), not has_down_host, "hostId"))
+        test_topology.append(Host("b", "2.2.3.%d" % i, "us", "cluster1", "rac{}".format(i % 3), True, "hostId"))
+        test_topology.append(Host("c", "3.2.3.%d" % i, "eu", "cluster2", "rac{}".format(i % 3), True, "hostId"))
+        test_topology.append(Host("d", "4.2.3.%d" % i, "us", "cluster2", "rac{}".format(i % 3), True, "hostId"))
     return Topology(test_topology)
 
 
